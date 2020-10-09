ca8a271ec561763ca2eda3354f538ffe32f7d4bd
Pbsmith82 <pbsmith82@gmail.com>
2020-10-09 14:20:49 -0400

    Created the API Call and started Job Class

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 class API
 
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+        
+        
+    def get_response_body    
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+ binding.pry
 end
--- lib/job.rb
+++ lib/job.rb
@@ -1,3 +1,5 @@
 class Job
 
+    attr_accessor :id, :type, :created_at, :company, :location, :job_title, :job_description, :how_to-apply
+
 end
